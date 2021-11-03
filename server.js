eae87aaa02cd6d36fc8482925d0e0567cff48de5
pfrueh1 <fruehpaul@gmail.com>
2021-11-03 10:43:26 -0400

    1st bd query

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        password:'$eaDZVn$i5eG',
+        database:'election'
+    },
+    console.log('connected to the election database')
+)
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+  console.log(rows);
+});
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
